e0ff3b0efc885fa6ba5adb7862c4d4017d85b617
Laura Evans <lauraevans@lauras-air.home>
2020-03-19 09:52:05 -0500

    attempted

--- balancedBrackets/balancedBracets.js
+++ balancedBrackets/balancedBracets.js
@@ -9,4 +9,29 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+    let parentheses = "(){}[]";
+    let stack = [];
+      //iterate through str
+    for(let i = 0; i < str.length ; i++) {
+        let character = str[i];
+        let bracePosition = parentheses.indexOf(character);
+  
+      if(bracePosition === -1) {
+          //continue? 
+        continue;
+      }
+  
+      if(bracePosition % 2 === 0) {
+        stack.push(bracePosition + 1); // push next expected brace position
+      } else {
+        if(stack.length === 0 || stack.pop() !== bracePosition) {
+          return false;
+        }
+      }
+    }
+  
+    return stack.length === 0;
+};
+//can't get it return false
+console.log(isBalanced([((({}50]));
